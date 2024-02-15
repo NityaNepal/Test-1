9ef8eba9bbfa2fc5c87c2f643ef0e4bc0d0431f4
NityaNepal <159408073+NityaNepal@users.noreply.github.com>
2024-02-15 14:31:04 +0600

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+print("hello ice")
+print("hi tashi")
+grade = 90
+if grade >= 90:
+    print("grade A")
+elif grade >= 80:
+    print("grade b")
+elif grade >= 70:
+    print("grade c")
+else:
+    print("grade d")
